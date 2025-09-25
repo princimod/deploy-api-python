5fd50b3ec8b48afe34c5e48dcda33fd0e006c44a
princimod <princimod@gmail.com>
2025-09-25 19:00:06 +0100

    Commit inicial com estrutura do app e configurações do docker

--- app/main.py
+++ app/main.py
@@ -5,7 +5,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Olá, mundo! 🚀"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
